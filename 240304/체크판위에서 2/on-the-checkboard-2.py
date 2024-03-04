728499edfc2709a79c6bff9408c4e433543d100d
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-04 22:15:21 +0900

    Title: 체크판위에서 2, Time: 221ms, Memory: 25MB, Status: Passed - CodeTree

--- 240304/체크판위에서 2/on-the-checkboard-2.py
+++ 240304/체크판위에서 2/on-the-checkboard-2.py
@@ -5,22 +5,23 @@ input = sys.stdin.readline
 r,c = map(int,input().split())
 
 checkMap = []
-for _ in range(c):
+for _ in range(r):
     checkMap.append(list(map(str,input().split())))
 
 start = checkMap[0][0]
+
 end = checkMap[-1][-1]
 answer = 0
 if start == end:
     print(answer)
 else:
-    for i in range(1,c-2):
+    for i in range(1,r-2):
         check = 0
-        for j in range(1,r-2):
+        for j in range(1,c-2):
             if checkMap[i][j] != start:
                 check += 1
-                for k in range(i+1,c-1):
-                    for l in range(j+1,r-1):
+                for k in range(i+1,r-1):
+                    for l in range(j+1,c-1):
                         if checkMap[k][l] != end:
                             answer += 1
     print(answer)
