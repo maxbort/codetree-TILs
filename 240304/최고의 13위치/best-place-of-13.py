40b7953be87cf299797adb2fea067422fa416218
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-04 21:43:46 +0900

    Title: 최고의 13위치, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240304/최고의 13위치/best-place-of-13.py
+++ 240304/최고의 13위치/best-place-of-13.py
@@ -10,8 +10,8 @@ for _ in range(n):
 
 answer = 0
 for i in range(n):
-    tmp = 0
     for j in range(n-2):
+        tmp = 0
         for k in range(3):
             if tile[i][j+k] == 1:
                 tmp += 1
