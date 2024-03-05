8a08e1417bb53ddd9feb997cf180934ea21e9800
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-05 16:41:12 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 145ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240305/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+point = []
+for _ in range(n):
+    point.append(list(map(int,input().split())))
+
+
+dist = float('inf')
+
+for i in range(1,n-1):
+    start = point[0]
+    end = point[-1]
+    tmp = 0
+    for j in range(0,i-1):
+        tmp += abs(point[j][0] - point[j+1][0]) + abs(point[j][1] - point[j+1][1])
+    tmp += abs(point[i-1][0] - point[i+1][0]) + abs(point[i-1][1] - point[i+1][1])
+    for k in range(i+1,n-1):
+        tmp += abs(point[k][0] - point[k+1][0]) + abs(point[k][1] - point[k+1][1])
+    
+    dist = min(dist,tmp)
+print(dist)
