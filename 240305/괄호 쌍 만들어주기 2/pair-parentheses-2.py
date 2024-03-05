4909cce0959f1138b66e2ef4d8c8bd7de7403526
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-05 18:25:42 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 170ms, Memory: 25MB, Status: Passed - CodeTree

--- 240305/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 240305/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -1,31 +1,12 @@
-import sys
+# 변수 선언 및 입력
+string = input()
+n = len(string)
 
-input = sys.stdin.readline
-
-bracket = list(map(str,input().rstrip()))
-
-stack = []
-answer = 0
-n = len(bracket)
-for i in range(n):
-    check = True
-    if bracket[i] == '(':
-        stack.append(bracket[i])
-        for k in range(i+1,n):
-            if bracket[k] == '(':
-                stack.append(bracket[k])
-                for j in range(i+1,n):
-                    if bracket[j] == ')':
-                        stack.pop()
-                    if not stack:
-                        answer+= 1
-                        check = False
-                        break
-                if stack:
-                    print(answer)
-                    exit()
-                
-            if not check:
-                stack = []
-                break
-print(answer)
+# 모든 쌍을 다 잡아봅니다.
+cnt = 0
+for i in range(n - 1):
+    for j in range(i + 1, n - 1):
+        if string[i] == '(' and string[i + 1] == '(' and string[j] == ')' and string[j + 1] == ')':
+            cnt += 1
+            
+print(cnt)
