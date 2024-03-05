fe8b1e1a258586b441af99441a6f0fe9d09c0ef3
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-05 18:21:58 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240305/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 240305/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -2,22 +2,26 @@ import sys
 
 input = sys.stdin.readline
 
-bracket = list(map(str,input()))
-n = len(bracket)
+bracket = list(map(str,input().rstrip()))
 
+stack = []
 answer = 0
+n = len(bracket)
 for i in range(n):
-    stack = []
+    check = True
     if bracket[i] == '(':
         stack.append(bracket[i])
-        for j in range(i+1,n):
-            if len(stack) == 1:
-                stack.append(bracket[j])
-            if bracket[j] == ')':
-                if len(stack) == 2:
-                    stack.append(bracket[j])
-                if len(stack) == 3:
-                    answer += 1
-                    break
-
+        for k in range(i+1,n):
+            if bracket[k] == '(':
+                stack.append(bracket[k])
+                for j in range(i+1,n):
+                    if bracket[j] == ')':
+                        stack.pop()
+                    if not stack:
+                        answer+= 1
+                        check = False
+                        break
+            if not check:
+                stack = []
+                break
 print(answer)
