45ebf78be60682107e462c21a63c3c1aba754873
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-06 21:18:20 +0900

    Title: 구간 중 최대 합, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240306/구간 중 최대 합/max-sum-of-subarray.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+n,k = map(int,input().split())
+
+num_list = list(map(int,input().split()))
+
+answer = 0
+for i in range(n-k):
+    tmp = 0
+    for j in range(k):
+        tmp += num_list[i+j]
+    answer = max(answer,tmp)
+print(answer)
