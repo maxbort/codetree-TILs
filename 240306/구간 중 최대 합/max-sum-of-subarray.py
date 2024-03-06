b7229f8257bff3c114e7df8cf16b363f13d01637
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-06 21:18:42 +0900

    Title: 구간 중 최대 합, Time: 108ms, Memory: 25MB, Status: Passed - CodeTree

--- 240306/구간 중 최대 합/max-sum-of-subarray.py
+++ 240306/구간 중 최대 합/max-sum-of-subarray.py
@@ -7,7 +7,7 @@ n,k = map(int,input().split())
 num_list = list(map(int,input().split()))
 
 answer = 0
-for i in range(n-k):
+for i in range(n-k+1):
     tmp = 0
     for j in range(k):
         tmp += num_list[i+j]
