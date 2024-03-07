bd82e4df498bc894775004b7fd38586d754ba581
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-07 19:16:25 +0900

    Title: 바구니 안의 사탕 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240307/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240307/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -10,7 +10,7 @@ for _ in range(n):
     bag[point] += candy
 
 answer = 0
-for i in range(k,100-k):
+for i in range(k,100-k+1):
     tmp = sum(bag[i-k:i+k+1])
     answer = max(answer,tmp)
 print(answer)
