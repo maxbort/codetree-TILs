d1ddc244843e0aad1860f8a2d89ddd63ea453ba8
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-07 20:00:20 +0900

    Title: 최고의 33위치, Time: 146ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240307/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+graph = [list(map(int,input().split())) for _ in range(n)]
+
+answer = 0
+for i in range(n-2):
+    for j in range(n-2):
+        tmp = 0
+        for k in range(3):
+            for l in range(3):
+                tmp+= graph[i+k][j+l]
+    answer = max(tmp,answer)
+
+print(answer)
