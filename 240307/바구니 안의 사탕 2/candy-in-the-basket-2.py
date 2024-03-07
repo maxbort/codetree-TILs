578d28bc0806bc61c700c61223fc2b9c4825d2c6
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-07 19:09:05 +0900

    Title: 바구니 안의 사탕 2, Time: 117ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240307/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+n,k = map(int,input().split())
+
+bag = [0 for _ in range(100)]
+for _ in range(n):
+    candy, point = map(int,input().split())
+    bag[point] = candy
+
+answer = 0
+for i in range(k,100-k):
+    tmp = sum(bag[i-k:i+k+1])
+    answer = max(answer,tmp)
+print(answer)
