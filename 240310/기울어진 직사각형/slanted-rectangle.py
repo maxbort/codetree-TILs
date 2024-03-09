837b45999dec25910b049b0e5de3e08e542936bb
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-10 02:13:22 +0900

    Title: 기울어진 직사각형, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240310/기울어진 직사각형/slanted-rectangle.py
@@ -0,0 +1,71 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+graph = [list(map(int,input().split())) for _ in range(n)]
+
+answer = 0
+
+for i in range(n):
+    for j in range(n):
+        tmp = 0
+        start = graph[i][j]
+        x,y = i,j
+        check1 = 0
+        check2 = 0
+        while True:
+            dx = x - 1
+            dy = y + 1
+            if 0 <= dx < n and 0 <= dy < n:
+                check1+= 1
+                tmp += graph[dx][dy]
+                x,y = dx,dy
+            else:
+                break
+
+        if check1 == 0:
+            continue
+
+        while True:
+            dx = x - 1
+            dy = y - 1
+            if 0 <= dx < n and 0 <= dy < n:
+                tmp += graph[dx][dy]
+                check2+= 1
+                x,y = dx,dy
+            else:
+                break
+
+        if check2 == 0:
+            continue
+        
+        while check1 > 0:
+            dx = x + 1
+            dy = y - 1
+            if 0 <= dx < n and 0 <= dy < n:
+                x,y = dx,dy
+                check1 -= 1
+                tmp += graph[dx][dy]
+            else:
+                break
+        if check1 != 0:
+            continue
+
+        while check2 > 0:
+            dx = x + 1
+            dy = y + 1
+            if 0 <= dx < n and 0 <= dy < n:
+                x,y = dx,dy
+                tmp += graph[dx][dy]
+                check2 -= 1
+            else:
+                break
+
+        if check2 !=0:
+            continue
+       
+        answer = max(answer,tmp)
+
+print(answer)
