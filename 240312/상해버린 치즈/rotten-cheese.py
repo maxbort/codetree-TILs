38cf49047dfdb3033e8b138a46dccd29e9d0b148
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-12 02:43:47 +0900

    Title: 상해버린 치즈, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240312/상해버린 치즈/rotten-cheese.py
+++ 240312/상해버린 치즈/rotten-cheese.py
@@ -9,21 +9,27 @@ eat_when=[list(map(int,input().split())) for _ in range(d)]
 sick_when = [list(map(int,input().split())) for _ in range(s)]
 
 sick_when.sort(key=lambda x : x[1])
-fisrt_sick = sick_when[0][1]
 candi_bad_cheeze = [False for _ in range(m+1)]
 cheeze_cnt = [0 for _ in range(m+1)]
+fisrt_sick = sick_when[0][1]
 
 for people_num, cheeze_num, eat_time in eat_when:
     for sick_people, sick_time in sick_when:
         if people_num == sick_people:
-            if eat_time < fisrt_sick:
+            if sick_time > fisrt_sick:
+                continue
+        if people_num == sick_time:
+            if eat_time < sick_time:
                 candi_bad_cheeze[cheeze_num] = True
                 cheeze_cnt[cheeze_num] += 1
 
-tmp = 0
+
+
+
+tmp= 0
 bad_cheeze = 0
 for i in range(len(cheeze_cnt)):
-    if cheeze_cnt[i] > tmp:
+    if cheeze_cnt[i] >= tmp:
         tmp = cheeze_cnt[i]
         bad_cheeze = i
 
