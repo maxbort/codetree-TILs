0373eabe12489c4554ac8d68a59dca71654d6745
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-11 22:09:20 +0900

    Title: 상해버린 치즈, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240311/상해버린 치즈/rotten-cheese.py
+++ 240311/상해버린 치즈/rotten-cheese.py
@@ -8,25 +8,40 @@ eat_when=[list(map(int,input().split())) for _ in range(d)]
 
 sick_when = [list(map(int,input().split())) for _ in range(s)]
 
-cheeze_list = [False for _ in range(m+1)]
+cheeze_list = [0 for _ in range(m+1)]
 people_list = [i for i in range(n+1)]
 
-eat_when.sort(key=lambda x : x[2])
+
+
 for sick_pople,sick_time in sick_when:
     for people_num, cheeze_num, eat_time in eat_when:
         if people_num == sick_pople:
             if eat_time < sick_time:
-                cheeze_list[cheeze_num] = True
-            else:
-                cheeze_list[cheeze_num] = False
-    
-answer = [0 for _ in range(n+1)]
+                cheeze_list[cheeze_num] += 1
+
+
+tmp = [0 for _ in range(m+1)]
 for i in range(len(cheeze_list)):
     if cheeze_list[i]:
-        tmp = 0
         for people_num,cheeze_num,eat_time in eat_when:
             if cheeze_num == i:
-                answer[people_num] = 1
+                for j in sick_when:
+                    if people_num == j[0]:
+                        tmp[i] += 1
+
+k = max(tmp)
+candi_list = []
+
+for i in range(len(cheeze_list)):
+    if cheeze_list[i] == k:
+        candi_list.append(i)
+tmp_list = [0 for _ in range(m+1)]
+for i in candi_list:
+    cnt = 0
+    for people_num, cheeze_num, eat_time in eat_when:
+        if i == cheeze_num:
+            cnt+=1
+    tmp_list[i] = cnt
 
 
-print(sum(answer))
+print(max(tmp_list))
