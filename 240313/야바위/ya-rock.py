2768716c0235474418500dd1b38bcc328318d5f3
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-13 01:40:07 +0900

    Title: 야바위, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240313/야바위/ya-rock.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+simul = [list(map(int,input().split())) for _ in range(n)]
+answer = 0
+
+
+for i in range(2):
+    cups = [0,0,0]
+    cups[i] = 1
+    tmp = 0
+    for a,b,c in simul:
+        for _ in range(n):
+            cups[a-1], cups[b-1] = cups[b-1], cups[a-1]
+        if cups[c-1] == 1:
+            tmp += 1
+    answer = max(tmp,answer) 
+print(answer)
