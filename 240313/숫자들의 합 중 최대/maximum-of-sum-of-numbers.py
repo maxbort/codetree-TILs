7ed4be7524ce3cfaf76eeb7f9aa7caba11b6db5a
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-13 00:07:06 +0900

    Title: 숫자들의 합 중 최대, Time: 111ms, Memory: 25MB, Status: Passed - CodeTree

--- 240313/숫자들의 합 중 최대/maximum-of-sum-of-numbers.py
+++ 240313/숫자들의 합 중 최대/maximum-of-sum-of-numbers.py
@@ -4,11 +4,10 @@ input = sys.stdin.readline
 
 x, y = map(str,input().rstrip().split())
 
-sum_x = 0
-sum_y = 0
-for i in x:
-    sum_x += int(i)
-for j in y:
-    sum_y += int(j)
-
-print(max(sum_x,sum_y))
+answer = 0
+for i in range(int(x),int(y)+1):
+    tmp = 0
+    for j in str(i):
+        tmp += int(j)
+    answer = max(answer,tmp)
+print(answer)
