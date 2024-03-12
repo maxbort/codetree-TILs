b86b6aafbaf55c4153a60cbe1b7458c7c2059ca7
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-13 00:45:19 +0900

    Title: 정보에 따른 숫자 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240313/정보에 따른 숫자 2/number-based-on-information-2.py
@@ -0,0 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+
+t,a,b = map(int,input().split())
+
+line = ['O' for _ in range(b+1)]
+
+for _ in range(t):
+    c,d = map(str,input().split())
+
+    line[int(d)] = c
+
+answer = 0
+for i in range(a,b+1):
+    d1 = b+1
+    d2 = b+1
+    for j in range(a,b+1):
+        if line[j] == 'S':
+            d1 = min(d1,abs(j-i))
+        if line[j] == 'N':
+            d2 = min(d2,abs(j-i))
+
+    if d2 >= d1:
+        answer += 1
+    
+print(answer)
