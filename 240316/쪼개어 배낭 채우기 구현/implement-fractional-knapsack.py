25e7085c74b3ca18f9ffd76aefaf15affb227e2e
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-16 10:57:59 +0900

    Title: 쪼개어 배낭 채우기 구현, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240316/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.py
@@ -0,0 +1,28 @@
+import sys
+
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+
+jew = []
+
+for _ in range(n):
+    a,b = map(int,input().split())
+    jew.append([b,round(b/a,3),a])
+
+jew.sort(key = lambda x : x[1], reverse=True)
+
+answer= 0
+for rv,v,w in jew:
+    if w < m:
+        answer += rv
+        m -= w
+    else:
+        for _ in range(w):
+            answer += v
+            m-=1
+            if m == 0:
+                break
+        if m == 0:
+            break
+print("{:.3f}".format(round(answer,3)))
