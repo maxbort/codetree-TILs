e2ed5ccdfdb9e8a646b5adffda922dd1e54fdd26
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-16 11:30:21 +0900

    Title: 숫자 합치기, Time: 190ms, Memory: 29MB, Status: Wrong Answer - CodeTree

--- 240316/숫자 합치기/merge-numbers.py
+++ 240316/숫자 합치기/merge-numbers.py
@@ -1,20 +1,36 @@
+import sys
+from copy import deepcopy
+
+input = sys.stdin.readline
+
 n = int(input())
-num = list(map(int, input().split()))
+ 
+num = list(map(int,input().split()))
+
 num.sort()
 
-answer = 0
-
-while len(num) > 1:
+answer=  0
+tmp =[]
+while True:
     tmp = []
-    for i in range(0, len(num)-1, 2):
-        tmp_sum = num[i] + num[i+1]
-        answer += tmp_sum
-        tmp.append(tmp_sum)
-    if len(num) % 2 != 0:
+    if len(num) % 2 == 0:    
+        for i in range(0,len(num),2):
+            tmp.append(num[i] + num[i+1])
+        answer += sum(tmp)
+
+    else:
+        for i in range(0,len(num),2):
+            if i < len(num)-1:
+                tmp.append(num[i] + num[i+1])
+        answer += sum(tmp)
         tmp.append(num[-1])
+
     num = tmp
 
-if len(num) == 1:
-    answer += num[0]
+    if len(num) == 2:
+        answer += sum(tmp)
+        break
+    if len(num) == 1:
+        break
 
 print(answer)
