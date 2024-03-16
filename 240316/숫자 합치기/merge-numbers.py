2217228544253747fcdaa249fd033a6ed2d5dd7d
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-16 12:00:32 +0900

    Title: 숫자 합치기, Time: 5072ms, Memory: 33MB, Status: Time Limit Exceed - CodeTree

--- 240316/숫자 합치기/merge-numbers.py
+++ 240316/숫자 합치기/merge-numbers.py
@@ -2,15 +2,14 @@ import sys
 from collections import deque
 
 input = sys.stdin.readline
-
+from collections import deque
 n = int(input())
 
 num = list(map(int,input().split()))
 
 num.sort(reverse = True)
-
 answer = 0
-while len(num) > 1:
+while len(num) > 2:
     tmp = []
     a = num.pop()
     b = num.pop()
@@ -18,5 +17,7 @@ while len(num) > 1:
     num.append(a+b)
     num.sort(reverse = True)
 
+if len(num) == 2:
+    answer += sum(num)
 
 print(answer)
