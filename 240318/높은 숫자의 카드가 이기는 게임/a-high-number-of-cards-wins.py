588c2dd73550a9f836c717fdf38f8100df9002f8
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-18 08:38:07 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240318/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
+++ 240318/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
@@ -5,11 +5,11 @@ input = sys.stdin.readline
 n = int(input())
 b_card = [int(input()) for _ in range(n)]
 
-a_card = [i+1 for i in range(2*n)]
+a_card = [i for i in range(2*n + 1)]
 
 answer = 0
 for i in b_card:
-    if i < 2*n-1:
+    if i < 2*n:
         if a_card[i+1] != 0:
             a_card[i+1] == 0
             answer += 1
