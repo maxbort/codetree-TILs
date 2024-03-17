4a492f5d23e771fefdc2bc442e3a1f34f2393bc8
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-18 08:32:43 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 163ms, Memory: 31MB, Status: Passed - CodeTree

--- 240318/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
+++ 240318/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
@@ -6,10 +6,14 @@ n = int(input())
 
 car_price = list(map(int,input().split()))
 
-answer= 0
-for i in range(n-1):
-    for j in range(i+1,n):
-        tmp = car_price[j] - car_price[i]
-        answer = max(tmp,answer)
+answer = 0
+min_price = car_price[0]
 
+for i in range(n):
+    tmp = car_price[i] - min_price
+
+    answer = max(answer,tmp)
+
+    if min_price > car_price[i]:
+        min_price = car_price[i]
 print(answer)
