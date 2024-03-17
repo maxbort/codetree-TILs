0745afe00bd338326cf807a049e8057c0aa1b50d
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-18 08:47:58 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 3531ms, Memory: 27MB, Status: Time Limit Exceed - CodeTree

--- 240318/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
+++ 240318/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
@@ -1,33 +1,21 @@
-# 변수 선언 및 입력:
+import sys
+
+input = sys.stdin.readline
+
 n = int(input())
-a_cards = []
-b_cards = [
-    int(input())
-    for _ in range(n)
-]
-# b 카드 숫자로 이루어진 hashset을 완성해줍니다.
-b_set = set(b_cards)
+b_card = [int(input()) for _ in range(n)]
+a_card = []
+answer = 0
+for i in range(1,2*n+1):
+    if i not in b_card:
+        a_card.append(i)
 
-# a 카드를 완성해줍니다.
-a_cards = [
-    num
-    for num in range(1, 2 * n + 1)
-    if num not in b_set
-]
-
-# a, b 카드 목록을 전부 오름차순으로 정렬해줍니다.
-a_cards.sort()
-b_cards.sort()
-
-# a의 카드를 작은 숫자부터 보며
-# b카드의 앞에서부터 이길 수 있는 순간에 둘을 매칭하는게 최선임을 이용합니다.
-ans = 0
+answer = 0
 b_idx = 0
+a_card.sort()
+b_card.sort()
 for a_idx in range(n):
-    # a가 현재 b 카드보다 우세하다면
-    # 둘을 매칭해주는게 항상 유리합니다.
-    if b_idx < n and a_cards[a_idx] > b_cards[b_idx]:
-        ans += 1
+    if b_idx < n and a_card[a_idx] > b_card[b_idx]:
+        answer += 1
         b_idx += 1
-
-print(ans)
+print(answer)
