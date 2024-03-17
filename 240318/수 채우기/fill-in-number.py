946a2893d039ffe022c3ff31664896c33d3f9b2f
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-18 08:11:09 +0900

    Title: 수 채우기, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240318/수 채우기/fill-in-number.py
+++ 240318/수 채우기/fill-in-number.py
@@ -9,22 +9,21 @@ coin = [5,2]
 answer = 0
 
 if len(n) > 1:
-    if n[-1] == '1' or n[-1] == '3':
-        if n[-1] == '1':
-            n = int(n) - 21
-            answer += 6
-            for i in coin:
-                a = n //i
-                answer += a
-                n -= a * i
-        else:
-            n = int(n) - 13
-            answer += 5
-            for i in coin:
-                a = n //i
-                answer += a
-                n -= a * i
-    if n[-1] == '8':
+    if n[-1] == '1':
+        n = int(n) - 21
+        answer += 6
+        for i in coin:
+            a = n //i
+            answer += a
+            n -= a * i
+    elif n[-1] == '3':
+        n = int(n) - 13
+        answer += 5
+        for i in coin:
+            a = n //i
+            answer += a
+            n -= a * i
+    elif n[-1] == '8':
         n = int(n) - 8
         answer += 4
         for i in coin:
