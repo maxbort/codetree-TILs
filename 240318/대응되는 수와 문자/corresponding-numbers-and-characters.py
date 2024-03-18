b31de235d0f56fab2b90203a6ece067711de08c4
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-18 12:15:27 +0900

    Title: 대응되는 수와 문자, Time: 308ms, Memory: 42MB, Status: Passed - CodeTree

--- 240318/대응되는 수와 문자/corresponding-numbers-and-characters.py
+++ 240318/대응되는 수와 문자/corresponding-numbers-and-characters.py
@@ -5,17 +5,14 @@ input = sys.stdin.readline
 n, m = map(int,input().split())
 
 di = {}
-
+di_num = {}
 for i in range(n):
     a = str(input().rstrip())
     di[a] = i+1
-
+    di_num[i+1] = a
 for _ in range(m):
     a = input().rstrip()
     if a.isdigit():
-        for k,v in di.items():
-            if v == int(a):
-                print(k)
-            
+        print(di_num.get(int(a)))
     else:
         print(di.get(a))
