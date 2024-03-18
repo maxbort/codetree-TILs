2b3b6730a38ffad026d7897fe1ac5b74082c1788
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-18 11:03:25 +0900

    Title: 자연수 M/2개의 쌍, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240318/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
+++ 240318/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
@@ -1,38 +1,64 @@
 import sys
-
+from collections import deque
 input = sys.stdin.readline
 
 n = int(input())
 
-num_set = {}
+num_set = []
 
 for _ in range(n):
     x,y = map(int,input().split())
 
-    num_set[y] = x
+    num_set.append([y,x])
+num_set.sort(key=lambda x : x[0])
+
+num_set = deque(num_set)
 
 max_sum = 0
-while num_set:
-    max_value = max(num_set.keys())
-    min_value = min(num_set.keys())
-    max_cnt = num_set.get(max_value)
-    min_cnt = num_set.get(min_value)
+while len(num_set) > 1:
+    max_value, max_cnt = num_set.pop()
+    min_value, min_cnt= num_set.popleft()
+   
 
     if max_cnt > min_cnt:
         max_cnt -= min_cnt
         max_sum = max(max_sum, max_value + min_value)
-        num_set.pop(min_value,None)
-        num_set[max_value] = max_cnt
+        num_set.append((max_value,max_cnt))
+        
 
     elif max_cnt == min_cnt:
-        num_set.pop(min_value,None)
-        num_set.pop(max_value,None)
         max_sum = max(max_sum, max_value + min_value)
     
     else:
         min_cnt -= max_cnt
         max_sum = max(max_sum, max_value + min_value)
-        num_set.pop(max_value,None)
-        num_set[min_value] = min_cnt
+        num_set.appendleft(min_value,min_cnt)
+
+
+    # max_value = max(num_set.keys())
+    # min_value = min(num_set.keys())
+    # max_cnt = num_set.get(max_value)
+    # min_cnt = num_set.get(min_value)
+
+    # if max_cnt > min_cnt:
+    #     max_cnt -= min_cnt
+    #     max_sum = max(max_sum, max_value + min_value)
+    #     num_set.pop(min_value,None)
+    #     num_set[max_value] = max_cnt
+
+    # elif max_cnt == min_cnt:
+    #     num_set.pop(min_value,None)
+    #     num_set.pop(max_value,None)
+    #     max_sum = max(max_sum, max_value + min_value)
     
+    # else:
+    #     min_cnt -= max_cnt
+    #     max_sum = max(max_sum, max_value + min_value)
+    #     num_set.pop(max_value,None)
+    #     num_set[min_value] = min_cnt
+
+if num_set:
+    last_value,last_cnt = num_set.pop()
+    max_sum = max(max_sum,last_value * 2)
+
 print(max_sum)
