a105cc1feaf3f71b6ccec447c639581f2e812499
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-18 09:56:07 +0900

    Title: 폭탄 해체 작업, Time: 422ms, Memory: 28MB, Status: Passed - CodeTree

--- 240318/폭탄 해체 작업/the-bomb-dismantling.py
+++ 240318/폭탄 해체 작업/the-bomb-dismantling.py
@@ -1,22 +1,24 @@
 import sys
+import heapq
 
+MAX_TIME = 10000
 input = sys.stdin.readline
 
 n = int(input())
-time_limit = 0
-bomb_list = []
+time_limit = 1
+bomb_list = [list(map(int,input().split())) for _ in range(n)]
 
-for _ in range(n):
-    a,b = map(int,input().split())
-    bomb_list.append([a,b])
+bomb_list.sort(key = lambda x : x[1])
 
-bomb_list.sort(key = lambda x : (-x[0],x[1]))
+pq = []
+bomb_idx = n-1
 answer = 0
 
-for i in bomb_list:
-    if time_limit > i[1]:
-        break
-    answer += i[0]
-    time_limit += 1
+for i in range(MAX_TIME, 0,-1):
+    while bomb_idx >= 0 and bomb_list[bomb_idx][1] >= i:
+        heapq.heappush(pq, -bomb_list[bomb_idx][0])
+        bomb_idx -= 1
     
+    if pq:
+        answer += -heapq.heappop(pq)
 print(answer)
