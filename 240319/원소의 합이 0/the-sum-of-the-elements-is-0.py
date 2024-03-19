646d3ab7f3b5ceaddb8b52c3e4f0aa666468aab4
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-19 10:27:15 +0900

    Title: 원소의 합이 0, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240319/원소의 합이 0/the-sum-of-the-elements-is-0.py
@@ -0,0 +1,37 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+a_list = list(map(int,input().split()))
+b_list = list(map(int,input().split()))
+c_list = list(map(int,input().split()))
+d_list = list(map(int,input().split()))
+
+dict_1 = {}
+dict_2 = {}
+
+for i in a_list:
+    for j in b_list:
+        a = i+j
+        if a in dict_1:
+            dict_1[a] += 1
+        else:
+            dict_1[a] = 1
+
+for i in c_list:
+    for j in d_list:
+        a = i+j
+        if a in dict_2:
+            dict_2[a] += 1
+        else:
+            dict_2[a] = 1
+
+answer = 0
+for k,v in dict_1.items():
+    want_know = -k
+    if want_know in dict_2:
+        tmp = dict_2[want_know]
+        answer += min(v,tmp)
+print(answer)
