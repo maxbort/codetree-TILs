02514a4f5998ccbccabff1c3e1d7f31b16ea2a53
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-19 11:23:29 +0900

    Title: 특별한 문자, Time: 242ms, Memory: 25MB, Status: Passed - CodeTree

--- 240319/특별한 문자/special-character.py
+++ 240319/특별한 문자/special-character.py
@@ -4,8 +4,15 @@ input = sys.stdin.readline
 
 word = input().rstrip()
 
+cnt = {}
 for i in word:
-    if word.count(i) == 1:
+    if i in cnt:
+        cnt[i] += 1
+    else:
+        cnt[i] = 1
+
+for i in word:
+    if cnt[i] == 1:
         print(i)
         sys.exit()
 print("None")
