d4490ce8cc64b196b7a74ae22869bac32d5348e3
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-20 10:08:21 +0900

    Title: 초대장과 번호표, Time: 110ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240320/초대장과 번호표/invitation-and-number-tag.py
@@ -0,0 +1,24 @@
+import sys
+
+input = sys.stdin.readline
+
+n,g =map(int,input().split())
+
+group=[list(map(int,input().split())) for _ in range(g)]
+group.sort(key=lambda x : len(x))
+invite = set()
+invite.add(1)
+
+for _ in range(2):
+    for i in group:
+        k = i[0]
+        num = i[1:]
+        not_invite =[]
+        for check in num:
+            if check in invite:
+                continue
+            else:
+                not_invite.append(check)
+        if len(not_invite) == 1:
+            invite.add(not_invite[0])
+print(len(invite))
