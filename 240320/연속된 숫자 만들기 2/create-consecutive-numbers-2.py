443689a292e0ac570b3e64ce2139fd49bc99abbc
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-20 10:57:09 +0900

    Title: 연속된 숫자 만들기 2, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240320/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -0,0 +1,36 @@
+import sys
+input = sys.stdin.readline
+
+a,b,c = map(int,input().split())
+
+answer = 0
+
+while True:
+    print(a,b,c)
+    if b-a == 1 and c-b == 1:
+        break
+    else:
+        if b-a < c-b:
+            if b-a == 1:
+                tmp = (b+c)//2
+                a = tmp
+                a,b = b,a
+                answer += 1
+            else:
+                tmp = (a+b) // 2
+                c = tmp
+                b,c = c,b
+                answer += 1
+        else:
+            if c-b == 1:
+                tmp = (a+b)//2
+                c = tmp
+                b,c = c,b
+                answer += 1
+            else:
+                tmp = (c+b) //2
+                a = tmp
+                a,b = b,a
+                answer += 1
+
+print(answer)
