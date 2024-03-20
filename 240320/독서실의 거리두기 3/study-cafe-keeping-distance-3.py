644212510ac57c8a36c18137f50dc0055bf2c051
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-20 11:54:36 +0900

    Title: 독서실의 거리두기 3, Time: 97ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240320/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
+++ 240320/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -20,6 +20,6 @@ a= max(dist,key= lambda x : x[1])
 b = min(dist,key=lambda x : x[1])
 answer = b[1]
 for i in range(a[0]+2, (a[0]+a[2])//2+1):
-    if  i-a[0] < answer and a[2]-i < answer:
+    if  i-a[0] <= answer and a[2]-i <= answer:
         answer = i-a[0]
 print(answer)
