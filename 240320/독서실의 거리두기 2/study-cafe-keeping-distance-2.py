57e4eb61453be7219bc226e646872a4531143d44
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-20 13:12:42 +0900

    Title: 독서실의 거리두기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240320/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 240320/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -9,17 +9,19 @@ seat = list(input().rstrip())
 
 dist = []
 tmp = [0,1,0]
+flag = False
 for i in range(1,n):
-    if i == n-1:
-        tmp[-1] = i
-        dist.append(tmp)
-    if seat[i] == '1':
-        tmp[-1] = i
-        dist.append(tmp)
-        tmp = [i,1,0]
-    else:
-        tmp[1]+= 1
 
+    if seat[i] == '1':
+        if flag:
+            tmp[-1] = i
+            dist.append(tmp)
+            tmp = [i,1,0]
+        else:
+            flag = True
+    else:
+        if flag:
+            tmp[1]+= 1
 first_p = 0
 last_p = 0
 for i in range(n):
@@ -38,11 +40,11 @@ answer = b[1]
 
 new_one = (a[0]+a[2])//2
 
-if seat[0] == '0':
+if seat[0] == '0' and seat[1] =='0':
     answer = min(answer,new_one-a[0],a[2]-new_one)
     temp = min(b[1],first_p)
     answer = max(temp,answer)
-elif seat[-1] == '0':
+elif seat[-1] == '0' and seat[-2] =='0':
     answer = min(answer,new_one-a[0],a[2]-new_one)
     temp = min(b[1],n-last_p-1)
     answer = max(temp,answer)
