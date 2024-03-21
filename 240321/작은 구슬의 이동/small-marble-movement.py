2a9b5631d0eb3b7fe1cc144db584ea9173c700bd
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-21 12:42:20 +0900

    Title: 작은 구슬의 이동, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240321/작은 구슬의 이동/small-marble-movement.py
+++ 240321/작은 구슬의 이동/small-marble-movement.py
@@ -4,38 +4,36 @@ input = sys.stdin.readline
 
 n,t = map(int,input().split())
 
-graph = [[0 for _ in range(n)] for _ in range(n)]
+x,y,d = map(str,input().rstrip().split())
 
-
-x,y,h = map(str,input().rstrip().split())
-
-x,y = int(x),int(y)
-
+x,y = int(x)-1,int(y)-1
 
 for _ in range(t):
-    if y == 0:
-        h = 'R'
+    if y < 0:
+        d = 'R'
         y = 1
-    elif y == n:
-        h = 'L'
-        y = n-1
-    elif x == 0:
-        h = 'D'
+        continue
+    elif y > n-1:
+        d = 'L'
+        y = n-2
+        continue
+    elif x < 0:
+        d = 'U'
         x = 1
-    elif x == n:
-        h = 'U'
-        x = n-1
-    if h == 'L':
-        y -= 1
-    elif h == 'R':
-        y += 1
-    elif h == 'D':
-        x -= 1
-    else:
-        x += 1
-    
-    
-    
-        
+        continue
+    elif x > n-1:
+        d = 'D'
+        x = n-2
+        continue
 
-print(x,y)
+
+    if d == 'L':
+        y -= 1
+    elif d == 'R':
+        y += 1
+    elif d == 'U':
+        x += 1
+    elif d == 'D':
+        x -= 1
+    
+print(x+1,y+1)
