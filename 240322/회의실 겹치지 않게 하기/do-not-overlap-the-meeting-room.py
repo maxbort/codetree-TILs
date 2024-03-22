fd221b6e63c4ef4b3492163b0e1c58004f80a805
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-22 21:40:32 +0900

    Title: 회의실 겹치지 않게 하기, Time: 5047ms, Memory: 27MB, Status: Time Limit Exceed - CodeTree

--- 240322/회의실 겹치지 않게 하기/do-not-overlap-the-meeting-room.py
+++ 240322/회의실 겹치지 않게 하기/do-not-overlap-the-meeting-room.py
@@ -11,7 +11,13 @@ m_list.sort(key=lambda x : (x[0],x[1]))
 answer = 0
 for i in range(n-1):
     now_s,now_e = m_list[i]
-    next_s,next_e = m_list[i+1]
-    if next_s <= now_e and next_e <= now_e:
-        answer += 1
+    check = 0
+    for j in range(i+1,n):
+        if check > 1:
+            answer += 1
+            break
+        next_s,next_e = m_list[i+1]
+        if next_s <= now_e and next_e <= now_e:
+            check += 1
+        
 print(answer)
