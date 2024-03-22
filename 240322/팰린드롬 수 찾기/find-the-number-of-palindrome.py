e68ce82b7cbc263fd4086c717b888a4eab031dbc
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-22 21:02:14 +0900

    Title: 팰린드롬 수 찾기, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240322/팰린드롬 수 찾기/find-the-number-of-palindrome.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+x,y = map(int,input().split())
+
+answer=  0
+for check in range(x,y+1):
+    check = str(check)
+    c_range = len(check)
+    flag = True
+    for i in range(c_range//2 + 1):
+        if check[i] != check[-i]:
+            flag = False
+            break
+    if flag:
+        answer += 1
+print(answer)
