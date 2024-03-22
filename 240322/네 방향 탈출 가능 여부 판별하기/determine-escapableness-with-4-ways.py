7ba30b4360eaa1922f86c33782eabffc3da5b0ba
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-22 11:13:14 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 203ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240322/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -0,0 +1,30 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+
+graph = [list(map(int,input().split())) for _ in range(n)]
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+q = deque()
+q.append((0,0))
+
+visited = [[False for _ in range(n)] for _ in range(n)]
+while q:
+    x,y = q.popleft()
+    visited[x][y] = True
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
+            q.append((nx,ny))
+
+if visited[-1][-1]:
+    print(1)
+else:
+    print(0)
