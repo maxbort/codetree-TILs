5404005ed73392dd35bf74e88e5d50e9a0d5c022
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-22 21:39:09 +0900

    Title: 회의실 겹치지 않게 하기, Time: 440ms, Memory: 28MB, Status: Wrong Answer - CodeTree

---
+++ 240322/회의실 겹치지 않게 하기/do-not-overlap-the-meeting-room.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+m_list = [list(map(int,input().split())) for _ in range(n)]
+
+m_list.sort(key=lambda x : (x[0],x[1]))
+
+answer = 0
+for i in range(n-1):
+    now_s,now_e = m_list[i]
+    next_s,next_e = m_list[i+1]
+    if next_s <= now_e and next_e <= now_e:
+        answer += 1
+print(answer)
