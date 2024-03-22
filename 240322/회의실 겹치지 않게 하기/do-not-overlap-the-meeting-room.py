05f686c8526d9da4c28782794b01241267bf8e4e
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-03-22 22:32:52 +0900

    Title: 회의실 겹치지 않게 하기, Time: 603ms, Memory: 31MB, Status: Passed - CodeTree

--- 240322/회의실 겹치지 않게 하기/do-not-overlap-the-meeting-room.py
+++ 240322/회의실 겹치지 않게 하기/do-not-overlap-the-meeting-room.py
@@ -1,25 +1,23 @@
-import sys
+def max_meetings(meetings):
+    # 끝나는 시간 기준으로 오름차순 정렬. 같은 경우 시작 시간으로 정렬.
+    sorted_meetings = sorted(meetings, key=lambda x: (x[1], x[0]))
+    last_end_time = 0
+    selected_meetings = 0
 
-input = sys.stdin.readline
+    for start, end in sorted_meetings:
+        if start >= last_end_time:  # 이전 회의가 끝난 후 시작되는 경우
+            selected_meetings += 1
+            last_end_time = end
 
+    # 전체 회의 수에서 선택된 회의 수를 빼서 취소해야 하는 회의 수를 구함
+    return len(meetings) - selected_meetings
+
+# 입력 예시
 n = int(input())
+meetings = []
+for _ in range(n):
+    s, e = map(int, input().split())
+    meetings.append((s, e))
 
-m_list = [list(map(int,input().split())) for _ in range(n)]
-
-m_list.sort(key=lambda x : (x[0],x[1]))
-
-answer = 0
-for i in range(n-1):
-    now_s,now_e = m_list[i]
-    check = 0
-    for j in range(i+1,n):
-        if check > 0:
-            answer += 1
-            break
-        next_s,next_e = m_list[i+1]
-        if next_s >= now_e:
-            break
-        if next_s < now_e and next_e <= now_e:
-            check += 1
-        
-print(answer)
+# 최소 취소 회의 수 출력
+print(max_meetings(meetings))
