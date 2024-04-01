b68d698900c813324e4e2f4c11159ccdb9ceb066
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-04-01 11:45:45 +0900

    Title: xor 결과 최대 만들기, Time: 306ms, Memory: 45MB, Status: Passed - CodeTree

--- 240401/xor 결과 최대 만들기/max-of-xor.py
+++ 240401/xor 결과 최대 만들기/max-of-xor.py
@@ -20,3 +20,24 @@ for candi in choose_num:
             tmp = tmp ^ k
     answer = max(tmp,answer)
 print(answer)
+
+### 백트래킹 방식
+
+# answer = 0
+# def find_max(curr_idx, cnt, curr_val):
+#     global answer
+
+#     if cnt == m:
+#         answer = max(answer,curr_val)
+#         return
+#     if curr_ix == n:
+#         return
+
+#     # curr_idx 숫자 선택 x
+#     find_max(curr_idx+1, cnt, curr_val)
+
+#     # curr_idx 숫자 선택 o
+#     find_max(curr_idx+1, cnt+1, curr_idx ^ n_list[curr_idx])
+
+# find_max(0,0,0)
+# print(answer)
