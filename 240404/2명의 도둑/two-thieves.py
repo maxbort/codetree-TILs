5d50bddc0353806cdc6dfef31883b2cafe204fd0
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-04-04 13:11:09 +0900

    Title: 2명의 도둑, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240404/2명의 도둑/two-thieves.py
+++ 240404/2명의 도둑/two-thieves.py
@@ -7,35 +7,55 @@ n,m,c = map(int,input().split())
 graph = [list(map(int,input().split())) for _ in range(n)]
 
 
-answer = 0
 
-def calValue(x,y):
+def calValue(x, y, tmp, cnt):
+    # 종료 조건이 되었을 때 현재 tmp에 대한 가치 계산 후 반환
+    if cnt == m-1:
+        value = sum(i*i for i in tmp)
+        return value
+
+    # tmp의 합이 c를 초과하면 마지막 값을 제거하고 계산
+    if sum(tmp) > c:
+        tmp.pop()
+        return sum(i*i for i in tmp) if tmp else 0
+
     value = 0
-    candi = graph[x][y:y+m]
-    candi.sort(reverse=True)
-    limit = 0
-    for i in candi:
-        limit += i
-        if limit <= c:
-            value += i*i
+    if y+cnt < len(graph[x]):
+        # 현재 위치의 값을 추가하는 경우
+        tmp.append(graph[x][y+cnt])
+        value_with = calValue(x, y, tmp, cnt+1)
+        tmp.pop()
+
+        # 현재 위치의 값을 추가하지 않는 경우
+        value_without = calValue(x, y, tmp, cnt+1)
+
+        # 두 경우 중 최댓값 선택
+        value = max(value_with if value_with is not None else 0, 
+                    value_without if value_without is not None else 0)
+
     return value
 
-def findMax():
-    if x1 == x2:
-        if y1 < n-m:
-            answer = max(answer)
 
 
+answer = 0
 for i in range(n):
-    for j in range(n-m):
+    for j in range(n-m+1):
         for k in range(n):
-            for l in range(n-m):
+            for l in range(n-m+1):
+                a = []
+                q,w = 0,0
                 if i == k:
-                    if i < n-2*m:
-                        answer = max(answer,calValue(i,j) + calValue(k,l))
-                    else:
-                        break
+                    if j <= n-2*m:
+                        l = j+m
+                        q = calValue(i,j,a,0)
+                        w = calValue(k,l,a,0)
+                        answer = max(answer,calValue(i,j,a,0) + calValue(k,l,a,0))
+
                 else:
-                    answer = max(answer,calValue(i,j) + calValue(k,l))
+                    q = calValue(i,j,a,0)
+                    w = calValue(k,l,a,0)
+                    answer = max(answer,calValue(i,j,a,0) + calValue(k,l,a,0))
+
+
 
 print(answer)
