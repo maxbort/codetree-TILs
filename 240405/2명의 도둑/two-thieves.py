512ee23e8147d64c6081b00022f56d637c8032b8
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-04-05 12:58:54 +0900

    Title: 2명의 도둑, Time: 139ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240405/2명의 도둑/two-thieves.py
+++ 240405/2명의 도둑/two-thieves.py
@@ -7,48 +7,36 @@ n,m,c = map(int,input().split())
 graph = [list(map(int,input().split())) for _ in range(n)]
 
 def calcValue(tmp):
-    value = 0
     return sum(i*i for i in tmp)
+
 answer = 0
 
 def findMax(x,y,x2,y2,cnt,tmp,tmp2):
     global answer
-    if sum(tmp) > c:
-        tmp.pop()
-        answer = max(answer,calcValue(tmp2)+calcValue(tmp))
+    if sum(tmp) > c or sum(tmp2) > c:
         return
-    
-    if cnt == m-1:        
+    if cnt == m:
         answer = max(answer,calcValue(tmp2)+calcValue(tmp))
         return 
-    if sum(tmp2) > c:
-        tmp2.pop()
-        answer = max(answer,calcValue(tmp2)+calcValue(tmp))
-        return
     
-  
-
-
     tmp.append(graph[x][y+cnt])
     findMax(x,y,x2,y2,cnt+1,tmp,tmp2)
     tmp2.append(graph[x2][y2+cnt])
     findMax(x,y,x2,y2,cnt+1,tmp,tmp2)
-
     tmp.pop()
     findMax(x,y,x2,y2,cnt+1,tmp,tmp2)
     tmp2.pop()
     findMax(x,y,x2,y2,cnt+1,tmp,tmp2)
 
-
 for i in range(n):
-    for j in range(n-m+1):
+    for j in range(n-m):
         for k in range(n):
-            for l in range(n-m+1):
+            for l in range(n-m):
                 if i == k:
-                    z = l+j+m
-                    if z > n-m+1:
+                    if j+m < l:
+                        findMax(i,j,k,l,0,[],[])
+                    else:
                         break
-                    findMax(i,j,k,l,0,[],[])
                 else:
                     findMax(i,j,k,l,0,[],[])
 
