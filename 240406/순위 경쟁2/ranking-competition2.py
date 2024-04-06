6854b69c0db7f0f29f3b7e5f6f2a263c42bac683
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-04-06 11:54:06 +0900

    Title: 순위 경쟁2, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

--- 240406/순위 경쟁2/ranking-competition2.py
+++ 240406/순위 경쟁2/ranking-competition2.py
@@ -13,29 +13,33 @@ rank['B'] = 0
 answer = 0
 tmp = 'AB'
 for user,score in rank_change:
-
     rank[user] += int(score)
     a_score = rank.get('A')
     b_score = rank.get('B')
     if a_score > b_score:
         if 'A' not in tmp:
-            tmp += 'A'
+            tmp = 'A'
             answer += 1
+
         else:
             if len(tmp) == 2:
                 tmp = 'A'
                 answer += 1
-    
+
     elif b_score > a_score:
         if 'B' not in tmp:
-            tmp += 'B'
+            tmp = 'B'
             answer += 1
+
         else:
             if len(tmp) == 2:
                 tmp = 'B'
                 answer += 1
+ 
     else:
         if len(tmp) != 2:
             answer += 1
             tmp = 'AB'
+
+    
 print(answer)
