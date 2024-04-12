8cf0987aecff8f0ad16df16cc4027b9219768cf8
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-04-12 18:05:40 +0900

    Title: 외판원 순회, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240412/외판원 순회/traveling-salesman-problem.py
@@ -0,0 +1,31 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+graph = [list(map(int,input().split())) for _ in range(n)]
+
+visited = [False for _ in range(n)]
+
+answer = float('inf')
+
+def findMin(cnt,cur,tmp):
+    global answer
+    if cnt == n-1:
+        tmp += graph[cur][0]
+        answer = min(answer,tmp)
+        return
+   
+    for i in range(1,n):
+        if visited[i] or graph[cur][i] == 0:
+            continue
+
+        tmp += graph[cur][i]
+        visited[i] = True
+        findMin(cnt+1,i,tmp)
+        tmp -= graph[cur][i]
+        visited[i] = False
+
+findMin(0,0,0)
+print(answer)
