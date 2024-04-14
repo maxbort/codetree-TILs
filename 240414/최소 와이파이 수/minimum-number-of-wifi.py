e87041a105461fd19d575a56fe0e3fd27cea4f8a
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-04-14 21:30:50 +0900

    Title: 최소 와이파이 수, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240414/최소 와이파이 수/minimum-number-of-wifi.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+
+people_point = list(map(int,input().split()))
+
+wifi_point = [False for _ in range(n)]
+
+answer = 0
+for i in range(n):
+    if people_point[i] == 0:
+        wifi_point[i] = True
+
+
+for i in range(n):
+    if not wifi_point[i]:
+        answer += 1
+        for j in range(i,i+2*m+1):
+            if 0 <= j < n:
+                wifi_point[j] = True
+            if j >= n-1:
+                print(answer)
+                sys.exit()
