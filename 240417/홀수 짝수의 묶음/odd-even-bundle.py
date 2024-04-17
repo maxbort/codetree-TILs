b51d4211df1101c8c7e07997fd2aedb216620f3a
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-04-17 11:15:25 +0900

    Title: 홀수 짝수의 묶음, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240417/홀수 짝수의 묶음/odd-even-bundle.py
@@ -0,0 +1,38 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+num_list = list(map(int,input().split()))
+
+even = 0
+odd = 0
+
+for num in num_list:
+    if num % 2 == 0:
+        even+= 1
+    else:
+        odd+=1
+
+if even == odd or even == odd + 1:
+    print(even+odd)
+else:
+    if even > odd:
+        while True:
+            even -= 1
+            if even + 1 == odd:
+                break
+        print(even + odd)
+    else:
+        while True:
+
+            odd -= 2
+            even += 1
+            if even+1 == odd:
+                odd -= 2
+                break
+            if even > odd:
+                break
+
+        
+        print(odd+even)
