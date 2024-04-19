52326099c0ce6430def2194bbc73e871a46f8875
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-04-19 12:11:15 +0900

    Title: 세 수의 최대 곱, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240419/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 240419/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -10,24 +10,60 @@ num_list.sort(key=lambda x : -abs(x))
 
 m_list = []
 p_list = []
+
 for i in num_list:
     if i >= 0 and len(p_list) < 3:
         p_list.append(i)
-    elif i < 0 and len(m_list) < 2:
+    elif i < 0 and len(m_list) < 3:
         m_list.append(i)
     
-    if len(p_list) == 3 and len(m_list) == 2:
+    if len(p_list) == 3 and len(m_list) == 3:
         break
 
-tmp1 = 1
-tmp2 = 1
+tmp1,tmp2 = -float('inf'), -float('inf')
 
-for i in p_list:
-    tmp1 *= i
-if len(m_list) == 2:
-    for j in m_list:
-        tmp2 *= j
-if p_list:
-    tmp2 *= p_list[0]
+if len(m_list) == 1:
+    tmp2 = 1
+    tmp2 *= m_list[0]
+    check = 0
+    for j in range(n-1,0,-1):
+        if num_list[j] >= 0:
+            tmp2 *= num_list[j]
+            check += 1
+        if check == 2:
+            break
+    
+
+
+elif len(m_list) >= 2:
+    tmp2 = 1
+
+    tmp2 *= m_list[0]
+    tmp2 *= m_list[1]
+    if p_list:
+        tmp2 *= p_list[0]
+    else:
+        tmp2 *= m_list[2]
+
+if len(p_list) == 1:
+    tmp1 = 1
+
+    tmp1 *= p_list[0]
+    tmp1 *= m_list[0]
+    tmp1 *= m_list[1]
+
+elif len(p_list) == 2:
+    tmp1 = 1
+    tmp1 *= p_list[0]
+    tmp1 *= p_list[1]
+    for j in range(n-1,0,-1):
+        if num_list[j] < 0:
+            tmp1 *= num_list[j]
+            break
+elif len(p_list) == 3:
+    tmp1 = 1
+    tmp1 *= p_list[0]
+    tmp1 *= p_list[1]
+    tmp1 *= p_list[2]
 
 print(max(tmp1,tmp2))
