a55b92f26943542f6be628f37d29f91e74ea64a0
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-07-22 20:00:28 +0900

    Title: 체크판위에서 2, Time: 155ms, Memory: 25MB, Status: Passed - CodeTree

--- 240722/체크판위에서 2/on-the-checkboard-2.py
+++ 240722/체크판위에서 2/on-the-checkboard-2.py
@@ -21,11 +21,10 @@ if arr[0][0] == arr[r-1][c-1]:
 else:
     for i in range(1, r-2):
         for j in range(1, c-2):
-            if arr[i][j] != arr[0][0]:
-                for a in range(i+1, r-1):
-                    for b in range(j+1, c-1):
-                        if arr[0][0] == arr[a][b]:
-                            cnt += 1
+            for a in range(i+1, r-1):
+                for b in range(j+1, c-1):
+                    if arr[i][j] != arr[0][0] and arr[0][0] == arr[a][b]:
+                        cnt += 1
 
 
 
