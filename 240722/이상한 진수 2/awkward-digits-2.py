ab0da51374154f382444b26fbd57284a62a66700
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-07-22 20:23:08 +0900

    Title: 이상한 진수 2, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240722/이상한 진수 2/awkward-digits-2.py
+++ 240722/이상한 진수 2/awkward-digits-2.py
@@ -1,23 +1,42 @@
-a = list(input())
+# a = list(input())
 
-one_cnt = 0
+# one_cnt = 0
 
-for i in a:
-    if i == '1':
-        one_cnt += 1
+# for i in a:
+#     if i == '1':
+#         one_cnt += 1
 
-if len(a) == one_cnt:
-    a[-1] = '0'
-else:
-    for i in range(len(a)):
-        if a[i] == '0':
-            a[i] = '1'
-            break
+# if len(a) == one_cnt:
+#     a[-1] = '0'
+# else:
+#     for i in range(len(a)):
+#         if a[i] == '0':
+#             a[i] = '1'
+#             break
 
-b = ''.join(a)
-ans = 0
+# b = ''.join(a)
+# ans = 0
 
-for i in range(len(b)-1, -1, -1):
-    if b[i] == '1':
-        ans += 2 ** (len(b) - i - 1)
+# for i in range(len(b)-1, -1, -1):
+#     if b[i] == '1':
+#         ans += 2 ** (len(b) - i - 1)
+# print(ans)
+
+import sys
+a = list(map(int, list(input())))
+
+ans = -sys.maxsize
+
+for i in range(len(a)):
+    a[i] = 1 - a[i]
+
+    cnt = 0
+    for j in range(len(a)):
+        if a[j] == 1:
+            cnt += 2 ** j
+    
+    ans = max(ans, cnt)
+
+    a[i] = 1 + a[i]
+
 print(ans)
