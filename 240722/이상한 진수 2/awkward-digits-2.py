2d9cd5244ce4150f4d17bb50e5321b479911968f
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-07-22 20:08:19 +0900

    Title: 이상한 진수 2, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240722/이상한 진수 2/awkward-digits-2.py
+++ 240722/이상한 진수 2/awkward-digits-2.py
@@ -1,9 +1,12 @@
 a = list(input())
 
-for i in range(len(a)):
-    if a[i] == '0':
-        a[i] = '1'
-        break
+if len(a) == 1:
+    a[0] = '0'
+else:
+    for i in range(len(a)):
+        if a[i] == '0':
+            a[i] = '1'
+            break
 
 b = ''.join(a)
 ans = 0
