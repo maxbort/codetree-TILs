86aa23a6ff3b45ee6ae02666182db02a6800711a
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-07-22 21:02:18 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240722/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 240722/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -1,40 +1,29 @@
 import sys
 n = int(input())
 
-# dist = [
-#     map(int,input().split())
-#     for i in range(n)
-# ]
-arr = []
-for _ in range(n):
-    x,y = map(int,input().split())
-    arr.append((x,y))
+arr = [
+    list(map(int,input().split()))
+    for i in range(n)
+]
 
 def get_dist(a,b):
     x1,y1 = a
     x2,y2 = b
     return abs(x1-x2) + abs(y1-y2)
 
-
-point = 1
 min_dist = sys.maxsize
+
 total_dist = 0
 for i in range(n-1):
     total_dist += get_dist(arr[i], arr[i+1])
 
-#print(min_dist)
-
 for i in range(1,n-1):
     dist = total_dist
-    # print("before  total_dist : {}".format(total_dist))
 
     dist -= get_dist(arr[i], arr[i+1])
     dist -= get_dist(arr[i-1], arr[i])
-    # print("minus dist : {}".format(dist))
     dist += get_dist(arr[i-1], arr[i+1])
-    # print("plus dist : {}".format(dist))
 
     min_dist = min(min_dist, dist)
-    # print("min_dist : {}".format(min_dist))
 
 print(min_dist)
