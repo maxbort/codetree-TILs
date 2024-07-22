60eec77002eda98f1c0cb281ee3443f2a78e2918
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-07-22 19:57:03 +0900

    Title: 체크판위에서 2, Time: 130ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240722/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+r,c = map(int,input().split())
+
+arr = [
+    list(input().split())
+    for _ in range(r)
+]
+
+d = [
+    [0 for _ in range(c)]
+    for _ in range(r)
+]
+
+q = deque()
+q.append((0,0))
+cnt = 0
+
+while q:
+    x,y = q.popleft()
+
+    for i in range(x+1, r-2):
+        for j in range(y+1, c-2):
+            if arr[i][j] != arr[x][y]:
+                for a in range(i+1, r-1):
+                    for b in range(j+1, c-1):
+                        if arr[x][y] == arr[a][b]:
+                            cnt += 1
+
+
+print(cnt)
