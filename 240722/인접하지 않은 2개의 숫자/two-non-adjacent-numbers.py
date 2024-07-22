b39f030d869fcd4998edfacfdb97a4124f9d62e8
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-07-22 21:24:50 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 138ms, Memory: 25MB, Status: Passed - CodeTree

--- 240722/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.py
+++ 240722/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.py
@@ -5,17 +5,9 @@ arr = list(map(int,input().split()))
 max_val = 0
 
 for i in range(n):
-    cnt = arr[i]
-    if i == 0:
-        cnt += max(arr[2:])
-    elif i == n-1:
-        cnt += max(arr[:-2])
-    else:
-        for j in range(1,n-1):
-            cnt = arr[i]
-            if abs(i-j) >= 2:
-                cnt += arr[j]
-            max_val = max(max_val, cnt)
-    
-    max_val = max(max_val, cnt)
+    val = arr[i]
+    for j in range(i+2, n):
+        val += arr[j]
+        max_val = max(max_val, val)
+        val -= arr[j]
 print(max_val)
