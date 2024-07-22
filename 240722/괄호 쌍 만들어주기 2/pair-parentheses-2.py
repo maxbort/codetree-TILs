7ece1f4a13060373879a1cd01b33475c21488d7a
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-07-22 21:15:34 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 240722/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 240722/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -2,9 +2,8 @@ string = input()
 
 cnt = 0
 for i in range(1,len(string)):
-    if string[i-1] == string[i] == '(':
-        for j in range(i+2, len(string)):
-            if string[j-1] == string[j] == ')':
-                cnt += 1
+    for j in range(i+2, len(string)):
+        if (string[i-1] == string[i] == '(') and (string[j-1] == string[j] == ')'):
+            cnt += 1
 
 print(cnt)
