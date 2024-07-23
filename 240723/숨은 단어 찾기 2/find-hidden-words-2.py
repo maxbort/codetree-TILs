6049016c56ea58791e68e8aa2f99303c593ecd68
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-07-23 20:03:49 +0900

    Title: 숨은 단어 찾기 2, Time: 114ms, Memory: 25MB, Status: Passed - CodeTree

--- 240723/숨은 단어 찾기 2/find-hidden-words-2.py
+++ 240723/숨은 단어 찾기 2/find-hidden-words-2.py
@@ -12,23 +12,19 @@ dxs,dys = [0,1,1,1,0,-1,-1,-1],[1,1,0,-1,-1,-1,0,1]
 def in_range(x,y):
     return 0<=x<n and 0<=y<m
 
-def check(x,y):
-    global cnt
-    for dx, dy in zip(dxs,dys):
-        flag = True
-
-        for i in range(1,3):
-            
-            if not in_range(x + dx * i, y + dy * i) or arr[x + dx * i][y + dy * i] != 'E':
-                flag = False
-                break
-        
-        if flag:
-            cnt += 1
-
 
 for i in range(n):
     for j in range(m):
         if arr[i][j] == "L":
-            check(i,j)
+            for dx, dy in zip(dxs,dys):
+                flag = True
+
+                for k in range(1,3):
+            
+                    if not in_range(i + dx * k, j + dy * k) or arr[i + dx * k][j + dy * k] != 'E':
+                        flag = False
+                        break
+        
+                if flag:
+                    cnt += 1
 print(cnt)
