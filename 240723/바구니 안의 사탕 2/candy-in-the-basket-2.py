0b650ef7d80c632c0f8119ef429a2617be88b6cb
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-07-23 21:38:08 +0900

    Title: 바구니 안의 사탕 2, Time: 106ms, Memory: 25MB, Status: Passed - CodeTree

--- 240723/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240723/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,12 +1,14 @@
 n,k = map(int,input().split())
 
-d = [0] * 101
+d = [0] * 401
 
 for _ in range(n):
     a,b = map(int,input().split())
     d[b] += a
 
 max_val = 0
-for i in range(k, 101-k):
-    max_val = max(max_val, sum(d[i-k:i+k+1]))
+for i in range(k, 401-k):
+    #print(max_val, sum(d[i-k:i+k+1]))
+    max_val = max(max_val, sum(d[i-k:i+k+1])) 
+
 print(max_val)
