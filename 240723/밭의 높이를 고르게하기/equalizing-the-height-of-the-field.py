dee9b1c5b5d2db66502423bdf2b23edc9e4c9c08
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-07-23 22:27:49 +0900

    Title: 밭의 높이를 고르게하기, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240723/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -0,0 +1,17 @@
+n,h,t = map(int,input().split())
+arr = list(map(int,input().split()))
+arr.sort()
+
+gap = []
+
+for i in arr:
+    gap.append(abs(i-h))
+
+#print(gap)
+
+gap.sort()
+
+cost = 0
+for i in range(h):
+    cost += gap[i] 
+print(cost)
