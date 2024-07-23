4ff7928bdb9539aca4f6d8a6474c5e231822bf6f
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-07-23 21:16:09 +0900

    Title: 아름다운 수열 2, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240723/아름다운 수열 2/beautiful-sequence-2.py
@@ -0,0 +1,22 @@
+from itertools import permutations as pm
+
+n,m = map(int,input().split())
+
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+
+b_pm = list(pm(b,len(b)))
+
+cnt = 0
+for i in range(n-len(b)+1):
+    for j in b_pm:
+        flag = True
+        for k in range(len(b)):
+            if j[k] != a[i:i+len(b)][k]:
+                flag = False
+                break
+
+        if flag:
+            cnt += 1
+
+print(cnt)
