8b76b24947649109226309a87213b7556ba09223
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-07-23 20:17:59 +0900

    Title: 특정 수와 근접한 합, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240723/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -0,0 +1,12 @@
+import sys
+
+n,s = map(int,input().split())
+arr = list(map(int,input().split()))
+
+min_gap = sys.maxsize
+total = sum(arr)
+
+for i in range(n-1):
+    for j in range(i+1,n):
+        min_gap = min(min_gap, total - arr[i] - arr[j])
+print(abs(s - min_gap))
