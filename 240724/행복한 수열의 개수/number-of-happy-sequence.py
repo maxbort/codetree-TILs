232209878a2dd676df360706e4df595db1b750e1
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-07-24 21:24:11 +0900

    Title: 행복한 수열의 개수, Time: 146ms, Memory: 25MB, Status: Passed - CodeTree

--- 240724/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240724/행복한 수열의 개수/number-of-happy-sequence.py
@@ -5,18 +5,29 @@ arr = [
 ]
 
 def array_cnt(temp_arr):
-    l = len(temp_arr)
+    # l = len(temp_arr)
     
-    for i in range(0,l-m+1):
-        cnt = 1
-        for j in range(i+1,l):
-            if temp_arr[i] == temp_arr[j]:
-                cnt += 1
-            else:
-                break
-        if cnt >= m:
-            return True
-    return False
+    # for i in range(0,l-m+1):
+    #     cnt = 1
+    #     for j in range(i+1,l):
+    #         if temp_arr[i] == temp_arr[j]:
+    #             cnt += 1
+    #         else:
+    #             break
+    #     if cnt >= m:
+    #         return True
+    # return False
+    seq_n, max_cnt = 1,1
+
+    for i in range(1,n):
+        if temp_arr[i] == temp_arr[i-1]:
+            seq_n += 1
+        else:
+            seq_n = 1
+        
+        max_cnt = max(max_cnt, seq_n)
+    
+    return max_cnt >= m
             
         
 
