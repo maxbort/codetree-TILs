43d4fdf54e45a0e7bc087a0e4d1846c24c0a5466
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-07-24 21:33:11 +0900

    Title: 트로미노, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240724/트로미노/tromino.py
@@ -0,0 +1,42 @@
+import sys
+n,m = map(int,input().split())
+
+arr = [
+    list(map(int,input().split()))
+    for _ in range(n)
+]
+
+max_val = 0
+
+def array_cnt(temp_arr):
+    global max_val
+    l = len(temp_arr)
+
+    val = 0
+    for i in range(0, l - 2):
+        for j in range(3):
+            val += temp_arr[i+j]
+        max_val = max(max_val, val)
+    
+    return max_val
+
+for i in range(n-1):
+    for j in range(m-1):
+        cnt = 0
+        min_val = sys.maxsize
+        for x in range(i,i+2):
+            for y in range(j,j+2):
+                cnt += arr[x][y]
+                min_val = min(min_val, arr[x][y])
+        
+        max_val = max(max_val , cnt - min_val)
+
+for i in range(n):
+    max_val = array_cnt(arr[i])
+
+    temp = []
+    for j in range(n):
+        temp.append(arr[j][i])
+    
+    max_val = array_cnt(temp)
+print(max_val)
