521083a0e6829bd360e764c95e648af43d6dea1f
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-07-25 22:07:35 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 716ms, Memory: 29MB, Status: Passed - CodeTree

--- 240725/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
+++ 240725/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
@@ -1,12 +1,13 @@
 import sys
+
 n,m = map(int,input().split())
+
+max_val = -sys.maxsize
 
 arr = [
     list(map(int,input().split()))
     for _ in range(n)
 ]
-
-max_val = -sys.maxsize
 
 visited = [
     [0] * m
@@ -18,6 +19,13 @@ def init_visited():
         for j in range(m):
             visited[i][j] = 0
 
+def overlap():
+    for i in range(n):
+        for j in range(m):
+            if visited[i][j] >= 2:
+                return True
+    return False
+
 def draw(x1,y1,x2,y2,x3,y3,x4,y4):
     for i in range(x1,x2+1):
         for j in range(y1,y2+1):
@@ -26,27 +34,19 @@ def draw(x1,y1,x2,y2,x3,y3,x4,y4):
         for j in range(y3,y4+1):
             visited[i][j] += 1
 
-def overlap():
-    for i in range(n):
-        for j in range(m):
-            if visited[i][j] >= 2:
-                return True
-    return False
-
 def check_overlap(x1,y1,x2,y2,x3,y3,x4,y4):
     init_visited()
     draw(x1,y1,x2,y2,x3,y3,x4,y4)
     return overlap()
 
-
-def rect_sum(x1,y1,x2,y2):
+def sum_rect(x1,y1,x2,y2):
     cnt = 0
     for i in range(x1,x2+1):
         for j in range(y1,y2+1):
             cnt += arr[i][j]
     return cnt
 
-def rect_total_sum(x1,y1,x2,y2):
+def total_sum_rect(x1,y1,x2,y2):
     max_val = -sys.maxsize
 
     for i in range(n):
@@ -54,14 +54,12 @@ def rect_total_sum(x1,y1,x2,y2):
             for x in range(i,n):
                 for y in range(j,m):
                     if not check_overlap(x1,y1,x2,y2,i,j,x,y):
-                        max_val = max(max_val, rect_sum(x1,y1,x2,y2) + rect_sum(i,j,x,y))
+                        max_val = max(max_val, sum_rect(x1,y1,x2,y2) + sum_rect(i,j,x,y))
     return max_val
-
 
 for i in range(n):
     for j in range(m):
         for x in range(i,n):
             for y in range(j,m):
-                max_val = max(max_val, rect_total_sum(i,j,x,y))
-
+                max_val = max(max_val, total_sum_rect(i,j,x,y))
 print(max_val)
