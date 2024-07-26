a655796da49121d8704892c14b9bb831653c1b82
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-07-26 14:59:21 +0900

    Title: 스승의 은혜 3, Time: 154ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240726/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -0,0 +1,26 @@
+n,b = map(int,input().split())
+
+price = [
+    list(map(int,input().split()))
+    for _ in range(n)
+]
+
+max_val = 0
+
+price.sort()
+
+for i in range(n):
+    val = 0
+    temp = b
+    price[i][0] //= 2
+
+    for j in range(n):
+
+        if temp - price[j][0] - price[j][1] >= 0:
+            val += 1
+            temp = temp - price[j][0] - price[j][1]
+    
+    max_val = max(max_val, val)
+    price[i][0] *= 2
+
+print(max_val)
