c28ba8d768ea9bcea2fd3e94364040282910353e
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-07-26 15:13:57 +0900

    Title: 스승의 은혜 3, Time: 5058ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

--- 240726/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240726/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -7,20 +7,22 @@ price = [
 
 max_val = 0
 
+
 for i in range(n):
     val = 0
     temp = b
-    price[i][0] //= 2
-
-    price.sort(key=lambda x : (x[0] + x[1]))
+    temp_arr = [item[:] for item in price]
+    temp_arr[i][0] //= 2
+    #print(temp_arr)
 
     for j in range(n):
+        temp_arr.sort(key=lambda x : (x[0] + x[1]))
 
-        if temp - price[j][0] - price[j][1] >= 0:
+        if temp - sum(temp_arr[j]) >= 0:
             val += 1
-            temp = temp - price[j][0] - price[j][1]
+            temp -= sum(temp_arr[j])
     
     max_val = max(max_val, val)
-    price[i][0] *= 2
+    #price[i][0] *= 2
 
 print(max_val)
