275f92cbc9b0e5d5e2ab1960122681dc6accdd6c
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-07-29 17:20:36 +0900

    Title: 3개의 선 2, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240729/3개의 선 2/three-lines-2.py
@@ -0,0 +1,36 @@
+n = int(input())
+
+arr = [
+    list(map(int,input().split()))
+    for _ in range(n)
+]
+
+arr.sort(key = lambda x : (x[0], x[1]))
+
+x_s, y_s = set(), set()
+for x,y in arr:
+    x_s.add(x)
+    y_s.add(y)
+
+line = []
+for i in x_s:
+    line.append([i, -1])
+for i in y_s:
+    line.append([-1, i])
+
+l = len(line)
+
+ans = 0
+for i in range(l-2):
+    for j in range(i+1, l-1):
+        for h in range(j+1):
+            flag = True
+            for a, b in arr:
+                if (line[i][0]==a or line[i][1]==b) or (line[j][0]==a or line[j][1]==b) or (line[h][0]==a or line[h][1]==b):
+                    continue
+                else:
+                    flag = False
+            
+            if flag:
+                ans = 1
+print(ans)
