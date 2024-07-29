24794aa9e10df29762e1208e73c7418e1c86c033
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-07-29 15:31:58 +0900

    Title: 빙산의 일각 2, Time: 110ms, Memory: 25MB, Status: Passed - CodeTree

--- 240729/빙산의 일각 2/the-tip-of-the-iceberg-2.py
+++ 240729/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -7,17 +7,31 @@ arr = [
 
 max_cnt = 0
 
+# for h in range(1,1001):
+#     flag = False
+#     cnt = 0
+#     for i in range(n):
+#         if arr[i] - h > 0:
+#             if not flag:
+#                 cnt += 1
+#                 flag = True
+#         else:
+#             flag = False
+
+#     max_cnt = max(max_cnt ,cnt)
+
+# print(max_cnt)
+
 for h in range(1,1001):
-    flag = False
+
     cnt = 0
-    for i in range(n):
-        if arr[i] - h > 0:
-            if not flag:
-                cnt += 1
-                flag = True
-        else:
-            flag = False
 
-    max_cnt = max(max_cnt ,cnt)
+    if arr[0] > h:
+        cnt += 1
 
+    for i in range(1,n):
+        if arr[i] > h and arr[i-1] <= h:
+            cnt += 1
+
+    max_cnt = max(max_cnt, cnt)
 print(max_cnt)
