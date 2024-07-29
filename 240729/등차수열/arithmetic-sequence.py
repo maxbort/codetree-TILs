cea4f93ed56e523b035a61b7384bb412dad44bee
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-07-29 14:57:34 +0900

    Title: 등차수열, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

--- 240729/등차수열/arithmetic-sequence.py
+++ 240729/등차수열/arithmetic-sequence.py
@@ -1,10 +1,12 @@
 n = int(input())
 arr = list(map(int,input().split()))
 
-cnt = 0
-for k in range(1,51):
+max_cnt = 0
+for k in range(1,101):
+    cnt = 0
     for i in range(n-1):
         for j in range(i+1,n):
             if arr[j]  - k == k - arr[i]:
                 cnt += 1
-print(cnt)
+    max_cnt = max(max_cnt, cnt)
+print(max_cnt)
