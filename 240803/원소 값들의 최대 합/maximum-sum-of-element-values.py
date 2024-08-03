be98e499239b55c2939ed97626dcf39f5e271b7c
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-08-03 11:09:12 +0900

    Title: 원소 값들의 최대 합, Time: 153ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240803/원소 값들의 최대 합/maximum-sum-of-element-values.py
+++ 240803/원소 값들의 최대 합/maximum-sum-of-element-values.py
@@ -1,20 +1,18 @@
-import heapq
+from collections import deque
 
 n,m = map(int,input().split())
 arr = list(map(int,input().split()))
 
 
-def heapsort(arr):
-	h = []
-	res = []
-	for v in arr:
-		heapq.heappush(h,-v) #차례로 삽입
-	for i in range(len(h)):
-		res.append(-heapq.heappop(h))
-	return res
+q = deque(sorted(arr, reverse=True))
 
-cnt = 0
-for i in range(m):
-    cnt += heapsort(arr)[i]
+idx, cnt = 0,0
+while q:
+    data = q.popleft()
 
+    cnt += data
+    idx += 1
+
+    if idx == m:
+        break
 print(cnt)
