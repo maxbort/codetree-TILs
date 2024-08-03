a0e50d4f1510788bc6b00da8d738e3abfd990cc9
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-08-03 11:52:23 +0900

    Title: Date to Date, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240803/Date to Date/date-to-date.py
+++ 240803/Date to Date/date-to-date.py
@@ -1,7 +1,7 @@
 m1,d1,m2,d2 = map(int,input().split())
 
 num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-cnt = 0
+cnt = 1
 
 while True:
     if m1 == m2 and d1 == d2:
@@ -11,7 +11,7 @@ while True:
     d1 += 1
 
     if d1 > num_of_days[m1]:
-        d1 = 0
+        d1 = 1
         m1 += 1
 
 print(cnt)
