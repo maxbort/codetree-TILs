ac350d2d110b5c99d1d3d5c07c174bb9e2b15ffb
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-08-03 13:29:11 +0900

    Title: 연속되는 수 2, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240803/연속되는 수 2/continuous-number2.py
+++ 240803/연속되는 수 2/continuous-number2.py
@@ -5,14 +5,18 @@ arr = [
     for _ in range(n)
 ]
 
-max_cnt = 0
-cnt = 1
-for i in range(1,n):
-    if arr[i] == arr[i-1]:
-        cnt += 1
-    else:
-        max_cnt = max(max_cnt, cnt)
-        cnt = 1
-    
 
-print(max_cnt)
+if n == 1:
+    print(1)
+else:
+    max_cnt = 0
+    cnt = 1
+    for i in range(1,n):
+        if arr[i] == arr[i-1]:
+            cnt += 1
+        else:
+            max_cnt = max(max_cnt, cnt)
+            cnt = 1
+        
+
+    print(max_cnt)
