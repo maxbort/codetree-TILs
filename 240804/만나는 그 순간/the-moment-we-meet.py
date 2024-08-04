3f4d1eea08e4b52832cdb89cf2d6aef795a84a11
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-08-04 10:52:49 +0900

    Title: 만나는 그 순간, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240804/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,42 @@
+n,m = map(int,input().split())
+
+A = [0] * 1001
+B = [0] * 1001
+
+a_idx = 1
+
+for i in range(n):
+    d, t = input().split()
+    t = int(t)
+
+    if d == 'R':
+        for _ in range(t):
+            A[a_idx] = A[a_idx - 1] + 1
+            a_idx += 1
+    else:
+        for _ in range(t):
+            A[a_idx] = A[a_idx - 1] - 1
+            a_idx += 1
+
+
+a_idx = 1
+
+for i in range(m):
+    d, t = input().split()
+    t = int(t)
+
+    if d == 'R':
+        for _ in range(t):
+            B[a_idx] = B[a_idx - 1] + 1
+            a_idx += 1
+    else:
+        for _ in range(t):
+            B[a_idx] = B[a_idx - 1] - 1
+            a_idx += 1
+
+cnt = -1
+for i in range(1,1001):
+    if A[i] == B[i]:
+        cnt = i
+        break
+print(cnt)
