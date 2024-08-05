025f6b4d2f7051ec45bc3d9f7e10e54453cf80e0
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-08-05 11:40:43 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240805/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,25 @@
+n,k,p,t = map(int,input().split())
+
+arr = [
+    list(map(int,input().split()))
+    for _ in range(t)
+]
+
+arr.sort(key = lambda x : x[0])
+
+handShake = [0] * (n+1) # k번 이상 악수했는지 체크용
+virus = [0] * (n+1) # 감염되면 1
+virus[p] = 1
+
+for t,x,y in arr:
+    if virus[x] == 1:
+        if handShake[x] < k:
+            virus[y] = 1
+            handShake[x] += 1
+    elif virus[y] == 1:
+        if handShake[y] < k:
+            virus[x] = 1
+            handShake[y] += 1
+
+for i in range(1,n+1):
+    print(virus[i],end='')
