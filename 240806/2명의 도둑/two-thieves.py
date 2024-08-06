d0c939718004798c786fc4a180d4ef2fb0d2e844
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-08-06 16:22:55 +0900

    Title: 2명의 도둑, Time: 785ms, Memory: 29MB, Status: Passed - CodeTree

--- 240806/2명의 도둑/two-thieves.py
+++ 240806/2명의 도둑/two-thieves.py
@@ -5,26 +5,8 @@ arr = [
     for _ in range(n)
 ]
 
-max_val = 0
 ans = []
-
-def in_range(x,y):
-    return 0<=x<n and 0<=y<n
-
-def possible(x1,y1,x2,y2):
-    
-    if y1 + m - 1 >= n or y2 + m - 1 >= n:
-        return False
-
-    if x1 != x2:
-        return True
-    
-    if interact(y1, y1+m-1, y2, y2+m-1):
-        return False
-    return True
-
-def interact(a,b,c,d):
-    return not (b < c or d < a)
+max_val = 0
 
 def choose(idx, weight, value):
     global max_val
@@ -32,25 +14,39 @@ def choose(idx, weight, value):
     if idx == m:
         if weight <= c:
             max_val = max(max_val, value)
-        return 
+        return
     
     choose(idx+1, weight, value)
-    choose(idx+1, weight + ans[idx], value + ans[idx]** 2)
+    choose(idx+1, weight + ans[idx], value + ans[idx] ** 2)
 
 def find_max(x,y):
     global ans, max_val
 
-    max_val = 0
     ans = arr[x][y:y+m]
+    max_val = 0
     choose(0,0,0)
 
     return max_val
 
+def interact(a,b,c,d):
+    return not ((d < a) or (b < c))
 
-for i in range(n):
-    for j in range(n):
-        for x in range(n):
-            for y in range(n):
-                if possible(i,j,x,y):
-                    max_val = max(max_val, find_max(i,j) + find_max(x,y))
+def possible(x1,y1,x2,y2):
+    if y1 + m - 1 >= n or y2 + m - 1 >= n:
+        return False
+    
+    if x1 != x2:
+        return True
+    
+    if interact(y1, y1 + m - 1, y2, y2 + m - 1):
+        return False
+    return True
+
+for x1 in range(n):
+    for y1 in range(n):
+        for x2 in range(n):
+            for y2 in range(n):
+                if possible(x1,y1,x2,y2):
+                    max_val = max(max_val, find_max(x1,y1) + find_max(x2,y2))
+
 print(max_val)
