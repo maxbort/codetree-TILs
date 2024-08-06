f723b809f1379696652740f9d3233407ba6cb205
최효준(최철호) <82080071+maxbort@users.noreply.github.com>
2024-08-06 15:57:43 +0900

    Title: 사다리 타기, Time: 183ms, Memory: 26MB, Status: Passed - CodeTree

--- 240806/사다리 타기/ladder-game.py
+++ 240806/사다리 타기/ladder-game.py
@@ -2,19 +2,12 @@ import sys
 
 n,m = map(int,input().split())
 
-# arr = []
-# for _ in range(m):
-#     a, b = map(int, input().split())
-#     arr.append((b, a - 1))
+arr = []
+for _ in range(m):
+    a, b = map(int, input().split())
+    arr.append((b, a - 1))
 
-# arr.sort()
-
-arr = [
-    tuple(map(int,input().split()))
-    for _ in range(m)
-]
-
-arr.sort(key = lambda x : x[1])
+arr.sort()
 
 min_val = m
 ans = []
@@ -23,9 +16,9 @@ def possible():
     num1 = [i for i in range(n+1)]
     num2 = [i for i in range(n+1)]
 
-    for idx, _ in arr:
+    for _, idx in arr:
         num1[idx], num1[idx+1] = num1[idx+1], num1[idx]
-    for idx, _ in ans:
+    for _, idx in ans:
         num2[idx], num2[idx+1] = num2[idx+1], num2[idx]
     
     for i in range(n):
@@ -36,15 +29,15 @@ def possible():
 def choose(cnt):
     global min_val, ans
 
-    if cnt == n:
+    if cnt == m:
         if possible():
             min_val = min(min_val, len(ans))
         return
     
-    for i,j in arr:
-        ans.append((i,j))
-        choose(cnt+1)
-        ans.pop()
+    ans.append(arr[cnt])
+    choose(cnt+1)
+    ans.pop()
+    choose(cnt+1)
 
 
 choose(0)
